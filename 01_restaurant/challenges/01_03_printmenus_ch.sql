9e11f8d265b38d097710777f2ebf43cc6ca4039a
Yagmur GULEC <38396892+YagmurGULEC@users.noreply.github.com>
2023-02-17 20:18:33 +0000

    Challenge 2,3,4

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,10 @@
 -- Create reports that will be used to make three menus.
 
 -- Create a report with all the items sorted by price (lowest to highest).
+SELECT Type,Name,Price,Description from Dishes ORDER BY Price;
 -- Create a report showing appetizers and beverages.
+SELECT Type,Name,Price,Description from Dishes WHERE type='Appetizer' or type='Beverage' ORDER BY type;
 -- Create a report with all items except beverages.
+SELECT Type,Name,Price,Description from Dishes WHERE type!='Beverage' ORDER BY type;
+
+
